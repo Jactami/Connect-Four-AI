5df28a2ee7909af406cff3655df553735ea14676
Christian Bartenstein <christian.bartenstein@stud.uni-bamberg.de>
2020-08-10 17:50:22 +0200

    turned project into git repository

---
+++ bot.js
@@ -0,0 +1,95 @@
+/**
+ * An AI using minimax to chose an optimal action
+ */
+class Bot {
+    constructor(val) {
+        this.val = val;
+        this.actions = [];
+    }
+
+    /**
+     * simualates a given amount of next turns from a given state
+     * returns the best possible action
+     */
+    think(state, depth) {
+        let finalScore = this.minimax(state, depth, depth, -Infinity, Infinity, true);
+        // statistically prefer action in the middle of array which is also the middle of the game board
+        let mean = (this.actions.length - 1) / 2;
+        let sd = 1.5;
+        let chosenAction = this.actions[constrain(round(randomGaussian(mean, sd)), 0, this.actions.length - 1)];
+
+        console.clear();
+        console.log("state score", finalScore);
+        console.log("possible actions", this.actions);
+        console.log("chosen action", chosenAction);
+
+        return chosenAction;
+    }
+
+    /**
+     * an implementation of a minimax algorithm (with alpha-beta-pruning)
+     * explores a game tree from given starting state to a given depth
+     * returns a set of the best, equally valued actions possible
+     */
+    minimax(state, depth, finalDepth, alpha, beta, maximizing) {
+        if (state.isWon()) {
+            // not sure if giving closer wins (/losses) a higher (/lower) score has noticeable influence
+            if (state.prevPlayer == this.val) {
+                state.score = 10000 * (depth + 1);
+            } else { // opponent won
+                state.score = -10000 * (depth + 1);
+            }
+            return state.score;
+        }
+
+        if (state.isDraw()) {
+            state.score = 0;
+            return state.score;
+        }
+
+        if (depth == 0) {
+            state.evalState();
+            return state.score;
+        }
+
+        state.calcChildren();
+
+        if (maximizing) {
+            let maxScore = -Infinity;
+            for (let child of state.children) {
+                let evalScore = this.minimax(child, depth - 1, depth, alpha, beta, false);
+                if (evalScore >= maxScore) {
+                    if (evalScore == maxScore && depth == finalDepth) {
+                        this.actions.push(child.action);
+                    } else if (evalScore > maxScore && depth == finalDepth) {
+                        this.actions = [child.action];
+                    }
+                    maxScore = evalScore;
+                }
+                state.score = maxScore;
+                alpha = max(alpha, evalScore);
+                if (beta < alpha)
+                    break;
+            }
+            return maxScore;
+        } else {
+            let minScore = Infinity;
+            for (let child of state.children) {
+                let evalScore = this.minimax(child, depth - 1, depth, alpha, beta, true);
+                if (evalScore <= minScore) {
+                    if (evalScore == minScore && depth == finalDepth) {
+                        this.actions.push(child.action);
+                    } else if (evalScore < minScore && depth == finalDepth) {
+                        this.actions = [child.action];
+                    }
+                    minScore = evalScore;
+                }
+                state.score = minScore;
+                beta = min(beta, evalScore);
+                if (beta < alpha)
+                    break;
+            }
+            return minScore;
+        }
+    }
+}
---
+++ main.js
@@ -0,0 +1,104 @@
+const COLUMNS = 7;
+const ROWS = 6;
+const SPACE = 100;
+
+const DEPTH = 7;
+
+let board;
+let botVal;
+let usrVal;
+let activePlayer;
+let gameFinished;
+let gamePaused;
+let bot;
+let state;
+
+function preload() {
+    // random player starts first game
+    botVal = 1;
+    usrVal = -1;
+    activePlayer = random([botVal, usrVal]);
+}
+
+function setup() {
+    console.clear();
+    createCanvas(COLUMNS * SPACE, ROWS * SPACE);
+    board = new GameBoard();
+    let opp;
+    activePlayer == botVal ? opp = usrVal : opp = botVal;
+    state = new GameState(board.grid, activePlayer, opp, NaN, NaN);
+    bot = new Bot(botVal);
+
+    gameFinished = false;
+    gamePaused = false;
+}
+
+function draw() {
+    board.showBoard();
+
+    if (gamePaused)
+        return;
+
+    // hover coin under current mouse position
+    if (!gameFinished && activePlayer == usrVal) {
+        let pos = board.getCoinPostion();
+        board.hoverCoin(pos.column, pos.row, usrVal);
+    }
+
+    // bot moves
+    if (!gameFinished && activePlayer == botVal) {
+        botAction();
+    }
+}
+
+// handles user input
+function mousePressed() {
+    if (activePlayer == botVal)
+        return;
+
+    let pos = board.getCoinPostion();
+
+    // try to insert coin and (if successful) evaluate game
+    if (board.insert(pos.column, pos.row, usrVal)) {
+        evalGame(pos.column, pos.row);
+    }
+}
+
+function botAction() {
+    // simulate next X possible turns (8 seems like a maximum regarding performance)
+    let action = bot.think(state, DEPTH, botVal);
+    if (board.insert(action.column, action.row, botVal)) {
+        evalGame(action.column, action.row)
+    }
+}
+
+function evalGame(column, row) {
+    let nextPlayer;
+    activePlayer == botVal ? nextPlayer = usrVal : nextPlayer = botVal;
+    state = new GameState(board.grid, nextPlayer, activePlayer, column, row);
+
+    if (state.isWon()) {
+        gameFinished = true;
+        let playerName = "";
+        activePlayer == botVal ? playerName = "Red" : playerName = "Yellow";
+        setTimeout(() => {
+            alert("Congratulations, " + playerName + "! You are the winner!");
+            setup();
+        }, 100);
+    } else if (state.isDraw()) {
+        gameFinished = true;
+        setTimeout(() => {
+            alert("Oh no! It is a draw...");
+            setup();
+        }, 100);
+    }
+
+    // next players turn (even if new game will started!)
+    activePlayer = nextPlayer;
+
+    // small input delay to make placing of coins "more natural"
+    gamePaused = true;
+    setTimeout(() => {
+        gamePaused = false;
+    }, 300);
+}
