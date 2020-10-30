9fe80ea290d2d7a5ef1b0eb55698c524c3fc0a21
Christian Bartenstein <christian.bartenstein@stud.uni-bamberg.de>
2020-10-30 10:45:31 +0100

    added log for calculation time and number of explored states

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ class Bot {
     constructor(val) {
         this.val = val;
         this.actions = [];
+        this.counter;
     }
 
     /**
@@ -12,16 +13,24 @@ class Bot {
      * returns the best possible action
      */
     think(state, depth) {
+        let t0 = new Date().getTime();
+        this.counter = 0;
+
         let finalScore = this.minimax(state, depth, depth, -Infinity, Infinity, true);
         // statistically prefer action in the middle of array which is also the middle of the game board
         let mean = (this.actions.length - 1) / 2;
         let sd = 1.5;
         let chosenAction = this.actions[constrain(round(randomGaussian(mean, sd)), 0, this.actions.length - 1)];
 
+        let t1 = new Date().getTime();
+        let deltaT = t1 - t0;
+
         console.clear();
         console.log("state score", finalScore);
         console.log("possible actions", this.actions);
         console.log("chosen action", chosenAction);
+        console.log("explored states", this.counter);
+        console.log("execution time", deltaT);
 
         return chosenAction;
     }
@@ -32,6 +41,8 @@ class Bot {
      * returns a set of the best, equally valued actions possible
      */
     minimax(state, depth, finalDepth, alpha, beta, maximizing) {
+        this.counter++;
+
         if (state.isWon()) {
             // not sure if giving closer wins (/losses) a higher (/lower) score has noticeable influence
             if (state.prevPlayer == this.val) {
